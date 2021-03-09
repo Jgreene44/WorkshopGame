3e145bc34e84aa6533bae955d328ff319e50c856
Jordan Greene <greenjor000@gmail.com>
2021-03-08 23:47:26 -0600

    Add project files.

---
+++ WorkshopGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WorkshopGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new WorkshopGame())
+                game.Run();
+        }
+    }
+}
---
+++ WorkshopGame/Spaceship.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Runtime.InteropServices;
+
+namespace WorkshopGame
+{
+    public class Spaceship
+    {
+
+        private GamePadState gamePadState;
+
+        private KeyboardState keyboardState;
+
+        private Texture2D texture;
+
+        private Vector2 position = new Vector2(200, 200);
+
+        private bool flipped;
+
+
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("playerShip");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //Console.WriteLine(Mouse.GetState());
+            gamePadState = GamePad.GetState(0);
+            keyboardState = Keyboard.GetState();
+
+            // Apply the gamepad movement with inverted Y axis
+            position += gamePadState.ThumbSticks.Left * new Vector2(1, -1);
+            if (gamePadState.ThumbSticks.Left.X < 0) flipped = true;
+            if (gamePadState.ThumbSticks.Left.X > 0) flipped = true;
+
+            // Apply keyboard movement
+            if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W)) position += new Vector2(0, -1);
+            if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S)) position += new Vector2(0, 1);
+            if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
+            {
+                position += new Vector2(-1, 0);
+                flipped = true;
+            };
+            if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
+            {
+                position += new Vector2(1, 0);
+                flipped = false;
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+            Vector2 direction = new Vector2(Mouse.GetState().X, Mouse.GetState().Y) - position;
+            float rotation = (float)Math.Atan2(direction.Y, direction.X);
+
+
+            SpriteEffects spriteEffects = (flipped) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            spriteBatch.Draw(texture, position, null, Color.White, (float)(rotation + (Math.PI * 0.5f)), new Vector2(50, 38), (float)0.5f, spriteEffects, 0);
+            
+        }
+    }
+}
